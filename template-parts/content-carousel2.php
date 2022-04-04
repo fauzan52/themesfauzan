2c824b9f1b94d404b34492fc36999ea94be3323c
Fauzan Alghifari <fauzanalghifari52@gmail.com>
2022-04-04 08:46:19 +0700

    WP 04.04.2022

--- template-parts/content-carousel2.php
+++ template-parts/content-carousel2.php
@@ -1,52 +1,108 @@
+<?php
+$SliderPostOne = new WP_Query(array(
+    'post_type' => 'berita',
+    'posts_per_page' => 1,
+));
+?>
+
+<?php
+$SliderPostTwo = new WP_Query(array(
+    'post_type' => 'berita',
+    'posts_per_page' => 1,
+    'offset' => 1,
+));
+?>
+
+<?php
+$SliderPostThree = new WP_Query(array(
+    'post_type' => 'berita',
+    'posts_per_page' => 1,
+    'offset' => 2,
+));
+?>
+
+<?php
+$SliderPostFour = new WP_Query(array(
+    'post_type' => 'berita',
+    'posts_per_page' => 1,
+    'offset' => 1,
+));
+?>
 
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
-        <?php for ($a = 0; $a < 4; $a++) { ?>
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <?php } ?>
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
     </ol>
     <div class="carousel-inner">
-<!--        <div class="carousel-item active">-->
-<!--            <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2020/04/11/08/26/nature-5029360_960_720.jpg"-->
-<!--                 alt="First slide">-->
-<!--            <div class="carousel-caption d-none d-md-block">-->
-<!--                <h3>AAAA</h3>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--        <div class="carousel-item">-->
-<!--            <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2020/04/11/08/26/nature-5029360_960_720.jpg"-->
-<!--                 alt="First slide">-->
-<!--            <div class="carousel-caption d-none d-md-block">-->
-<!--                <h3>BBBBB</h3>-->
-<!--            </div>-->
-<!--        </div>-->
-<!---->
-<!--        <div class="carousel-item">-->
-<!--            <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2020/04/11/08/26/nature-5029360_960_720.jpg"-->
-<!--                 alt="First slide">-->
-<!--            <div class="carousel-caption d-none d-md-block">-->
-<!--                <h3>CCCCC</h3>-->
-<!--            </div>-->
-<!--        </div>-->
-
-        <?php
-        $SliderPost = new WP_Query(array(
-            'post_type' => 'berita',
-            'posts_per_page' => 4,
-        ));
-        ?>
-        <?php if ($SliderPost->have_posts()) : ?>
-        <?php while ($SliderPost->have_posts()) :
-        $SliderPost->the_post(); ?>
         <div class="carousel-item active">
-            <img class="d-block w-100" src="<?= get_the_post_thumbnail_url(); ?>"
-                 alt="First slide">
-            <div class="carousel-caption d-none d-md-block">
-                <h3><?php echo wp_trim_words(get_the_title(), 15, ' ...'); ?></h3>
-            </div>
+            <?php if ($SliderPostOne->have_posts()) : ?>
+                <?php while ($SliderPostOne->have_posts()) :
+                    $SliderPostOne->the_post(); ?>
+                    <a href="<?php the_permalink(); ?>">
+                        <img class="d-block w-100" src="<?= get_the_post_thumbnail_url(); ?>"
+                             alt="First slide">
+                    </a>
+                    <div class="carousel-caption d-none d-md-block">
+                            <h3>
+                                <a href="<?php the_permalink(); ?>"><?php echo wp_trim_words(get_the_title(), 15, ' ...'); ?></a>
+                            </h3>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
         </div>
-        <?php endwhile; ?>
-        <?php endif; ?>
+        <div class="carousel-item">
+            <?php if ($SliderPostTwo->have_posts()) : ?>
+                <?php while ($SliderPostTwo->have_posts()) :
+                    $SliderPostTwo->the_post(); ?>
+                    <a href="<?php the_permalink(); ?>">
+                        <img class="d-block w-100" src="<?= get_the_post_thumbnail_url(); ?>"
+                             alt="First slide">
+                    </a>
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>
+                            <a href="<?php the_permalink(); ?>"><?php echo wp_trim_words(get_the_title(), 15, ' ...'); ?></a>
+                        </h3>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
+
+        <div class="carousel-item">
+            <?php if ($SliderPostThree->have_posts()) : ?>
+                <?php while ($SliderPostThree->have_posts()) :
+                    $SliderPostThree->the_post(); ?>
+                    <a href="<?php the_permalink(); ?>">
+                        <img class="d-block w-100" src="<?= get_the_post_thumbnail_url(); ?>"
+                             alt="First slide">
+                    </a>
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>
+                            <a href="<?php the_permalink(); ?>"><?php echo wp_trim_words(get_the_title(), 15, ' ...'); ?></a>
+                        </h3>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
+
+        <div class="carousel-item">
+            <?php if ($SliderPostFour->have_posts()) : ?>
+                <?php while ($SliderPostFour->have_posts()) :
+                    $SliderPostFour->the_post(); ?>
+                    <a href="<?php the_permalink(); ?>">
+                        <img class="d-block w-100" src="<?= get_the_post_thumbnail_url(); ?>"
+                             alt="First slide">
+                    </a>
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>
+                            <a href="<?php the_permalink(); ?>"><?php echo wp_trim_words(get_the_title(), 15, ' ...'); ?></a>
+                        </h3>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
     </div>
 
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
